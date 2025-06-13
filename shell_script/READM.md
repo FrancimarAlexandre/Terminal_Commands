548968e76dfb4adc76ac916af7962592e5425bc2
FrancimarAlexandre <francimaralexandredt@gmail.com>
2025-06-13 16:54:16 -0300

    iniciando shell script

---
+++ shell_script/READM.md
@@ -0,0 +1,1 @@
+# praticando shell script
---
+++ shell_script/update_system.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# verificando qual o so está sendo usado
+
+source /etc/os-release
+
+echo "Detectando distribuição Linux: $NAME"
+
+case $ID in
+	ubuntu|debian)
+		echo "Atualizando sistema com APT..."
+		sudo apt update && sudo apt upgrade -y
+		;;
+	arch)
+		echo "Atualizando sistema com Pacman..."
+		sudo pacman -Syu --noconfirm
+	;;
+	fedora)
+		echo "Atualizando sistema com DNF..."
+		sudo zypper update -y
+	;;
+	void)
+		echo "Atualizando sistema com XBPS"
+		sudo xbps-install -Su
+	;;
+	*)
+		echo "Distribuição não suportada: $ID"
+		exit 1
+		;;
+esac
+
+echo "Atualização concluída!"
